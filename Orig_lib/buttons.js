3a732ae62dabadbd93ec69a79682e400b2bd307b
Amir <baruch.amir@gmail.com>
2018-06-09 10:27:14 +0200

    reorganized temp buttons

---
+++ Orig_lib/buttons.js
@@ -0,0 +1,111 @@
+var request = require('request');
+var parser = require('./parser');
+const player = require('play-sound')();
+
+var buttons_ds_hardcopy = {
+  "buttons": [
+    {
+      "_id": "5b145578d179c0e3b3839e32",
+      value: "door bell button",
+      MAC: "fc:65:de:7b:a2:1c",
+      active: true,
+      actions: [
+        {
+          "_id": "5b145578d179c0e3b3839e33",
+          type: "ifttt",
+          key: "cT6lm_RqMdNT3IoJfLbUCY",
+          active: true
+        }, {
+          "_id": "5b145578d179c0e3b3839e34",
+          type: "ifttt",
+          key: "dquuU8uBMFHupi7Jincbjk",
+          active: false
+        }
+      ]
+    }
+  ]
+
+};
+
+function buttons() {
+  this.activeButtons = [];
+
+}
+
+buttons.prototype.execute = function(msg) {
+  console.log("Executing with execution at buttons.prototype.execute");
+  var key = this.getKey(msg)
+  if (key == -1) {
+    return console.log("Not a valid call");
+  };
+  console.log("Executing with key:", key);
+  var filteredButtons = this.fetch(key); // get from DB
+  if (filteredButtons.length > 0) { // only in case there are relevant entries
+    console.log("Button exit with key: ", key);
+    for (var j in filteredButtons[0].actions) {
+      if (filteredButtons[0].actions[j].active == true) {
+        this.triggerAction(filteredButtons[0].actions[j].type, filteredButtons[0].actions[j].key);
+      }
+    }
+  }else{ console.log("No button with key: ", key);}
+
+};
+
+buttons.prototype.getKey = function(msg) {
+  var obj = parser.parse(msg);
+  if (typeof obj.options.clientIdentifier.address == 'undefined' || this.activeButtons.indexOf(obj.options.clientIdentifier.address) != -1) {
+    return -1;
+  }
+  this.activeButtons.push(obj.options.clientIdentifier.address)
+  setTimeout(() => {
+    // remove the item from the array
+    var index = this.activeButtons.indexOf(obj.options.clientIdentifier.address);
+    if (index > -1) {
+      this.activeButtons.splice(index, 1);
+    }
+  }, 5000)
+  return obj.options.clientIdentifier.address;
+
+}
+
+buttons.prototype.fetch = function(key) {
+  var buttons = buttons_ds_hardcopy.buttons; // TODO: replace with call to DB
+  var results = [];
+  // console.log(buttons[0]);
+  for (var i in buttons) {
+    if (buttons[i].active == true && buttons[i].MAC == key) {
+      results.push(buttons[i]);
+    }
+  }
+  return results;
+};
+buttons.prototype.triggerAction = function(type, key) {
+  console.log("reached triggerAction with: ", key);
+
+  switch (type) {
+    case "ifttt":
+      this.ifttt(key);
+      break;
+    case "play":
+      this.play();
+      break;
+    default:
+
+  }
+};
+buttons.prototype.ifttt = function(key) {
+  var amirsID = "cT6lm_RqMdNT3IoJfLbUCY"
+  var url = "https://maker.ifttt.com/trigger/button_pressed/with/key/"
+  request.post(url + amirsID, function(err, res, body) {
+    if (!err && res.statusCode == 200) {
+      console.log("message sent");
+    } else {
+      console.log("message could not be sent: ", err);
+
+    }
+  })
+
+};
+buttons.prototype.play = function(path) {};
+
+module.exports = buttons;
--- Orig_lib/server.js
+++ Orig_lib/server.js
@@ -1,66 +1,18 @@
 const dgram = require('dgram');
-var parser = require('./parser');
 var request = require('request');
 const player = require('play-sound')();
+const buttons = require('./buttons');
 
 
 
 function Server() {
-  this._readyForNextMsg = true
-  _this = this;
+  var _buttons = new buttons();
   this.server = dgram.createSocket('udp4');
   this.server.on('message', (msg) => {
-    console.log('message arrived');
-    console.log('current state of this._readyForNextMsg: ', this._readyForNextMsg);
-
-    if (this._readyForNextMsg) {
-      console.log('message received: ',msg);
-      obj = parser.parse(msg);
-      if (typeof obj.options.clientIdentifier.address != 'undefined') {
-        return console.log("undefined Mac Address");
-      }else if (obj.options.clientIdentifier.address == 'fc:65:de:7b:a2:1c'){
-        this._readyForNextMsg = false;
-        console.log('found you and sending');
-        player.play('./rsc/338.mp3', (err) => {
-          if (err) console.log(`Could not play sound: ${err}`);
-        });
-
-        // Amir
-        request.post(
-            'https://maker.ifttt.com/trigger/button_pressed/with/key/cT6lm_RqMdNT3IoJfLbUCY', { json: { key: 'value' } },
-            function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    console.log(body)
-                }
-                console.log(response);
-            }
-        );
-        // Maya
-        request.post(
-            'https://maker.ifttt.com/trigger/button_pressed/with/key/dquuU8uBMFHupi7Jincbjk', { json: { key: 'value' } },
-            function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    console.log(body)
-                }
-                console.log(response);
-            }
-        );
-
-
-        setTimeout(() =>{
-          this._readyForNextMsg = true;
-          console.log('just set the state of this._readyForNextMsg: ', this._readyForNextMsg);
-        },5000);
-    }else{
-      console.log('Mac Address: ', obj.options.clientIdentifier.address);
-      console.log('not the MAC address you are after ');
-    }
-
-    }else {
-      console.log('i think i am still in timeout: ',this._readyForNextMsg);
-    }
+    _buttons.execute(msg);
 
   });
+
   this.server.on('listening', (msg) => {
     console.log('in listening');
 
